0a0a2519d0bccd33401399174425cbc87f9d9df7
carlos <rep.csantos@gmail.com>
2025-11-07 15:47:41 -0300

    bugs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,9 @@
-import { fileURLToPath } from 'url';
-import path from 'path';
-
-/** Corrige o escopo ESModules */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+// next.config.mjs
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
-  outputFileTracingRoot: __dirname,
-  output: 'standalone'
+  output: 'standalone', // mantém compatibilidade com Docker/Railway
 };
 
 export default nextConfig;
